b66432fb2f11450e9278888ab90c925cca7cfaa3
alita_kallyne <alytakallyne@gmail.com>
2024-09-25 14:32:06 -0300

    Classe ObjetoEnviaEmail via SMTP

---
+++ src/main/java/br/com/enviando_mail/ObjetoEnviaEmail.java
@@ -0,0 +1,74 @@
+package br.com.enviando_mail;
+
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class ObjetoEnviaEmail {
+	
+	
+    private String userName = "alitakallyne@gmail.com ";
+	
+	private String senha = "gsmz cbeq fsgu rjnn"; //Feito no geradpr de Senha De APPS do Google
+	//private String senha = "#kalitah97";
+
+	private String listaDestinatarios = "";
+	private String nomeRemetente = "";
+	private String assunto = "";
+	private String textoEmail = "";
+	
+	
+	
+	public ObjetoEnviaEmail(String listaDestinatarios, String nomeRemetente, String assunto, String textoEmail) {
+		this.listaDestinatarios = listaDestinatarios;
+		this.nomeRemetente = nomeRemetente;
+		this.assunto = assunto;
+		this.textoEmail = textoEmail;
+	}
+
+
+
+	public void enviarEmail()  throws Exception{
+				
+			Properties props = new Properties();
+		    props.put("mail.smtp.ssl.trust", "*");
+		    props.put("mail.smtp.host", "smtp.gmail.com"); //SERVIDOR GEMAIL GOOGLE
+		    props.put("mail.smtp.port", "465");// PORTA DO SERVIDOR
+		    props.put("mail.smtp.auth", "true"); //AUTORIZAÇÃO
+		    props.put("mail.smtp.starttls.enable", "true"); //AUTENTICAÇÃO
+		    props.put("mail.smtp.starttls.enable", "true"); //AUTENTICAÇÃO
+		    props.put("mail.smtp.socketFactory.port", "465"); //ESPECIFICA A PORTA A SER CONECTADA PELO SOCKET
+		    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); //CLASSE SOCKET DE CONEXÃO AO SMTP
+		    
+		    Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+	           
+		    	@Override
+		    	protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+		    		
+		    		return new javax.mail.PasswordAuthentication(userName, senha);
+		    	}
+	        });
+		    
+		    System.out.println(session);
+		    
+		    Address [] toUser = InternetAddress.parse(listaDestinatarios);
+		    
+		    Message message = new MimeMessage(session);
+		    message.setFrom(new InternetAddress(userName,nomeRemetente)); //QUEM ESTA ENVIANDO O EMAIL
+		    message.addRecipients(Message.RecipientType.TO, toUser); //EMAIL DE DESTINO
+		    message.setSubject(assunto); //ASSUNTO DO EMAIL
+		    message.setText(textoEmail);
+		    
+		    Transport.send(message);
+		    
+		    
+		    System.out.println("E-mail enviado com sucesso!");
+		    
+	}
+
+}
--- src/test/java/br/com/enviando_mail/AppTest.java
+++ src/test/java/br/com/enviando_mail/AppTest.java
@@ -1,14 +1,6 @@
 package br.com.enviando_mail;
 
 
-import java.util.Properties;
-
-import javax.mail.Address;
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 
 /**
@@ -16,55 +8,17 @@ import javax.mail.internet.MimeMessage;
  */
 public class AppTest {
     
-
-	private String userName = "email@gmail.com ";
-	
-	private String senha = "senha_teste"; //Feito no geradpr de Senha De APPS do Google
-	
-
 	
 	@org.junit.Test
-	public void testeEmail () {
+	public void testeEmail () throws Exception {
 		
-		try {
-		
-		Properties props = new Properties();
-	    props.put("mail.smtp.host", "smtp.gmail.com"); //SERVIDOR GEMAIL GOOGLE
-	    props.put("mail.smtp.port", "465");// PORTA DO SERVIDOR
-	    props.put("mail.smtp.auth", "true"); //AUTORIZAÇÃO
-	    props.put("mail.smtp.starttls.enable", "true"); //AUTENTICAÇÃO
-	    props.put("mail.smtp.starttls.enable", "true"); //AUTENTICAÇÃO
-	    props.put("mail.smtp.socketFactory.port", "465"); //ESPECIFICA A PORTA A SER CONECTADA PELO SOCKET
-	    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); //CLASSE SOCKET DE CONEXÃO AO SMTP
+		ObjetoEnviaEmail enviaEmail = new ObjetoEnviaEmail("alitakallyne@gmail.com",
+				"Curso JDev-Treinamento", 
+				"Testando envio de email com java",
+				"Esse texto é a descrição do meu email.");
 	    
-	    Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-           
-	    	@Override
-	    	protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-	    		
-	    		return new javax.mail.PasswordAuthentication(userName, senha);
-	    	}
-        });
-	    
-	    System.out.println(session);
-	    
-	    Address [] toUser = InternetAddress.parse("alytakallyne@gmail.com,alitakallyne@gmail.com");
-	    
-	    Message message = new MimeMessage(session);
-	    message.setFrom(new InternetAddress(userName)); //QUEM ESTA ENVIANDO O EMAIL
-	    message.addRecipients(Message.RecipientType.TO, toUser); //EMAIL DE DESTINO
-	    message.setSubject("CHEGOU EMAIL ENVIADO COM JAVA"); //ASSUNTO DO EMAIL
-	    message.setText("Olá programador, vc acabou de receber um email enviado com Java do curso Formação Java Web do Alex!");
-	    
-	    Transport.send(message);
-	    
-	    
-	    System.out.println("E-mail enviado com sucesso!");
-	    
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	    
+		enviaEmail.enviarEmail();
+	  
 	}
 	
 	 
